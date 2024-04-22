e9770d7d839309f1c80014b76188c80c97909593
Gratian Marian Boghean <boghean.gratian@inetum.com>
2024-04-22 07:44:53 +0200

    Basic skeleton of the menu

---
+++ MasterApplication/Feature/Md5HashFileGenerator/Md5HashFileGeneratorView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterApplication.Feature.Md5HashFileGenerator;
+
+/// <summary>
+/// Interaction logic for Md5HashFileGeneratorView.xaml
+/// </summary>
+public partial class Md5HashFileGeneratorView : UserControl
+{
+    public Md5HashFileGeneratorView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MasterApplication/Feature/Md5HashFileGenerator/Md5HashFileGeneratorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterApplication.Feature.Md5HashFileGenerator;
+
+internal class Md5HashFileGeneratorViewModel
+{
+}
